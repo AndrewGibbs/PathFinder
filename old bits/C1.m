8d23583c3cfac669bf0e5e315f34960ac00f3ee4
Unknown <andrewgibbs1987@gmail.com>
2019-01-23 15:28:01 +0000

    Added a guide, moved a load of files around so it's all self-contained
    Former-commit-id: 546abfc6c525a2e226a21806098299f018612cc5
    Former-commit-id: df19841f62f26d23098d68c2f2c3631a91e1d04d

---
+++ old bits/C1.m
@@ -0,0 +1,36 @@
+
+Npts=15;
+h=1;
+visuals = false;
+a=exp(9i*pi/8);
+b=exp(1i*pi/8);
+X=-10:h:10;
+Y=-10:h:10;
+% -----------------------------------
+A31 = @(x,y) A1(x,y,a,b,Npts,visuals);
+
+tic;
+xCount = 0;
+for x = X
+    xCount = xCount + 1;
+    yCount = 0;
+    fprintf('\nx=%.2f',x)
+    for y = Y
+        yCount = yCount + 1;
+        u(xCount,yCount) = A31(x,y);
+        fprintf('\n\ty=%2.2f\t%.2f %%', y, 100*(length(Y)*(xCount-1)+yCount)/(length(X)*length(Y)));
+        if visuals
+            title(sprintf('x=%d, y=%d', x, y));
+            imPathName=sprintf('PearceyImages\\x%dy%d',x,y);
+            imPathNamJHPG=sprintf('x%dy%d.jpg',x,y);
+            savefig(imPathName);
+            saveas(gcf,imPathNamJHPG);
+            close;
+        end
+    end
+end
+
+T=toc;
+fprintf('\n Time = %f\n',T);
+surf(X,Y,abs(u).');
+view([0 90]); colorbar; %shading interp;
