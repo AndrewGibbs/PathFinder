0c6b6332bc603870907760f21ccadd1011ec8990
Unknown <andrewgibbs1987@gmail.com>
2019-03-12 10:25:07 +0000

    Have changed contour def'n to align with theory, code runs, have not checked quadrature error yet
    Former-commit-id: 99582f26551deac586cd0e3536411114db7e565c

--- plotAll.m
+++ plotAll.m
@@ -1,6 +1,9 @@
 function plotAll(covers,contours)
 %plots all contours and covers
     for C=covers
+        if iscell(C)
+            C=C{1};
+        end
         plot(C);
         hold on;
     end
@@ -10,4 +13,3 @@ function plotAll(covers,contours)
     end
     hold off;
 end
-
