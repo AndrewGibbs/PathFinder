ce64eefbc5d5aa1124aacb32044d3729f8c56aac
Unknown <andrewgibbs1987@gmail.com>
2019-04-04 17:02:04 +0100

    made the test suite
    still waiting on puns from Marwah for the other sweets
    
    Former-commit-id: 04760461c6c43359522fd0acded3f1cf66c6b8e6 [formerly ffc0cfb246f9b94007cbcbd25ce5ada457019390] [formerly a7df1d7d44f164cabb1e3e539c427634f03238bb]
    Former-commit-id: c79989afbf6929be0cbb12f448b2464c4e314097
    Former-commit-id: 77fc12ce121120de41f4c885945e729a67c9f7af
    Former-commit-id: 316091d40d4343a083afe65d1a9095b61470dadd
    Former-commit-id: 58d99b68e30a421de0c45a446b9be774d680d90e

--- src/contours/getContours.m
+++ src/contours/getContours.m
@@ -1,15 +1,38 @@
 function contours = getContours(G, covers, infContour, valleys, clusters, clusterEndpoints)
 
     contours = [];
+%      for n = 1:length(covers)
+%         if ~infContour && ismember(n,[1 2])
+%             intervalEndpoint = covers(n).centre;
+%         else
+%             intervalEndpoint = [];
+%         end
+%         for xi = covers(n).steepestExits
+%             contours = [contours ContourSD(xi,G,covers(n),[covers(1:(n-1)) covers((n+1):end)],valleys,clusters,clusterEndpoints)];
+%         end
+%      end
      for n = 1:length(covers)
         if ~infContour && ismember(n,[1 2])
-            intervalEndpoint = covers(n).centre;
+            intervalEndpoint = covers{n}.centre;
         else
             intervalEndpoint = [];
         end
-        for xi = covers(n).steepestExits
-            contours = [contours ContourSD(xi,G,covers(n),[covers(1:(n-1)) covers((n+1):end)],valleys,clusters,clusterEndpoints)];
+        for xi = covers{n}.steepestExits
+            contours = [contours ContourSD(xi,G,covers{n},coversComplement(n),valleys,clusters,clusterEndpoints)];
         end
      end
+     
+     
+    function coversOut = coversComplement(n)
+        numCovers = length(covers);
+        coversOut = [];
+        for m=1:(n-1)
+            coversOut{m}=covers{m};
+        end
+        for m=(n+1):numCovers
+            coversOut{m-1}=covers{m};
+        end
+     end
+
 end
 
