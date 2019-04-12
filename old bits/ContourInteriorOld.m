b11407eb3230771b477ed9cc0f2ad2921754fccb
Unknown <andrewgibbs1987@gmail.com>
2019-04-12 12:32:28 +0100

    Made a new level set function Not sure if it's more efficient than the old one
    Former-commit-id: 64891819492308bd82aa4d7a793882a5c8cc9759
    Former-commit-id: d4074eb2e7679b6da1b86c14afb9e4a7407af4a3

--- old bits/ContourInteriorOld.m
+++ old bits/ContourInteriorOld.m
@@ -4,34 +4,38 @@ classdef ContourInterior < ComplexCover
     properties
         boundary
         pseudoCentre
-        contourPts = 64
-        oscScale = 1
     end
     
     methods
-        function self = ContourInterior(pseudoCentre,freq,g,index,constScale, orderSum)
+        function self = ContourInterior(pseudoCentre,realRange,imagRange,freq,g,index,constScale, orderSum)
             if nargin == 6
                 constScale = 1;
             end
-            self.oscScale = constScale;
-            
             self.pseudoCentre = pseudoCentre;
             self.index = index;
-            c = getLevelContour(pseudoCentre, self.oscScale, g, freq, self.contourPts);
-            self.boundary.xdata = real(c);
-            self.boundary.ydata = imag(c);
+            Z = realRange(:).' + imagRange(:)*1i;
+            contourFix = @(z) freq*abs(g(pseudoCentre)-g(z));
+            c = contourc(realRange,imagRange,contourFix(Z),[constScale constScale]);
+            c_split = contourdata(c);
+            foundOurGuy = false;
+            openFlag = false;
+            for n = 1:length(c_split)
+               %put a test here to check that contour is closed
+               if c_split(n).isopen
+                   openFlag = true;
+               end
+               if inpolygon(real(pseudoCentre),imag(pseudoCentre),c_split(n).xdata,c_split(n).ydata)...
+                        && ~c_split(n).isopen
+                   self.boundary = c_split(n);
+                   foundOurGuy = true;
+               end
+            end
+            if ~foundOurGuy && openFlag
+                error('Could not determine contours, but at least one was open. Try a bigger region.');
+            end
             self.exitGrad = min(self.exitGrad,length(self.boundary.xdata));
             self.steepestExits = self.getSteepestExits(g);
             
-            self.boundaryComplex = self.boundary.xdata +1i*self.boundary.ydata;
-            
-            %to determine diameter, construct matrix of pairwise distances
-            %from self to self
-            x = self.boundary.xdata; y = self.boundary.ydata;
-            R = ((x-x.').^2 + (y-y.').^2);
-            self.diameter = max(max(R));
-            
-            self.orderSum = orderSum;
             self.boundaryComplex = self.boundary.xdata +1i*self.boundary.ydata;
             
             %to determine diameter, construct matrix of pairwise distances
