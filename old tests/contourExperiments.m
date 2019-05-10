dc86e0418bfa957b4a02d5053ffc6aa79701f080
Unknown <andrewgibbs1987@gmail.com>
2019-05-10 08:52:40 +0100

    Created a gitIgnore file... hopefully
    Former-commit-id: 04178450824f7f02399fd62f5838d1958505bec6 [formerly 904c12fac9d6faf149d9c127f7bb4c97b2e29469]
    Former-commit-id: 3b70f61a410cb55422e2d52c99719e510d5671d7
    Former-commit-id: 39ba8b01a12e18b0870609edda5f5e68ad6fec6c
    Former-commit-id: 4b7e9b2f0c4fe540cca228225288eb75bbe271ba

--- old tests/contourExperiments.m
+++
@@ -1,9 +0,0 @@
-C = .8;
-
-%g = @(z)  (z-1).*(z+1).*(z+1i);
-g = @(z)  (z-.1).*(z+.1).*(z+1.8i);
-x = linspace(-2.5,2.5,1000);
-z = x + 1i*x.';
-contour(x,x,abs(g(z)),[C C]);
-c = contourc(x,x,abs(g(z)),[C C]);
-c_split = contourdata(c);
