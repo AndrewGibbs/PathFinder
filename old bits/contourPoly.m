342028f6169e0a176944fdcc9abe65dc6711ec42
Unknown <andrewgibbs1987@gmail.com>
2019-05-10 08:52:40 +0100

    Created a gitIgnore file... hopefully
    Former-commit-id: 904c12fac9d6faf149d9c127f7bb4c97b2e29469
    Former-commit-id: 04178450824f7f02399fd62f5838d1958505bec6

--- old bits/contourPoly.m
+++
@@ -1,8 +0,0 @@
-function I = contourPoly(aVal,bVal,polyCoeffs,freq, Npts)
-%computes infinite contour integral from infiaVal to bVal of exp(i*freq*P(z)),
-%where P is a polynomial with coefficients given by polyCoeffs
-    R = monomialSettleRadius(polyCoeffs);
-    [ ~, W ] = PathFinder( aVal, bVal, freq, Npts, [], 'settleRad', R, 'ainf', 'binf', 'gpolycoeffs', polyCoeffs);
-    I = sum(W);
-    
-end
