c0dcc678062266724ee2f850bc95e86ba5495672
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2024-01-17 12:44:07 +0000

    styled filterPaths.m
    Former-commit-id: b2a9184b3b5c82974a076fc9d2ed0d0dcc551156

--- src/gauss_quadrature_rules/fliterPaths.m
+++ src/gauss_quadrature_rules/fliterPaths.m
@@ -1,40 +1,73 @@
-function [quadIngredients_out, max_val] = fliterPaths(quadIngredients,g,freq,thresh)
-    used_points = [];
-    quad_ingredient_index = [];
-    count = 0;
-    num_strln_samples = 15;
-    for q = quadIngredients
-        count = count + 1;
-        if ~isempty(q{1})
-            if strcmp(q{1}.type,'infSD')
-                used_points = [used_points q{1}.contour.startPoint];
-                quad_ingredient_index = [quad_ingredient_index count];
-            elseif strcmp(q{1}.type,'finSD')
+% Some contour integrals will have a negligable value compared to others.
+% This function removes those contours from the sequence, so quadrature
+% points are only allocated along non-negligable conoturs.
+
+% Here, information about each contour corresponds to a different
+% 'quadrature ingredient'.
+function [QuadIngredientArrayOut, maxIntegrand] =...
+            fliterPaths(QuadIngredientArray, phaseFunction, freq, thresh)
+
+    % initiliase a vector which stores key points along the deformation,
+    % where we expect the largest values of the ingrand to occur.
+    usedPoints = [];
+    % following assigns an index to each quadrature ingredient, and links
+    % this to the points in the above array
+    quadIngredientIndices = [];
+    % number of points to sample along straight line contours
+    num_strln_samples = 3;
+
+    % loop over array of structs describing contours
+    quadIndex = 0;
+    for quadIngredient = QuadIngredientArray
+        quadIndex = quadIndex + 1;
+        if ~isempty(quadIngredient{1}) % check ingredient is populated
+            
+            if strcmp(quadIngredient{1}.type,'infSD')
+            % if unbounded SD contour
+                usedPoints = [usedPoints quadIngredient{1}.contour.startPoint];
+                quadIngredientIndices = [quadIngredientIndices quadIndex];
+            elseif strcmp(quadIngredient{1}.type,'finSD')
+            % if bounded SD contour
                 % add both endpoints, as it's a finite path
-                used_points = [used_points q{1}.contour.startPoint q{1}.contour.endPoint];
-                quad_ingredient_index = [quad_ingredient_index count count];
-            elseif strcmp(q{1}.type,'strLn')
-                used_points = [used_points linspace(q{1}.a, q{1}.b, num_strln_samples)];
-                quad_ingredient_index = [quad_ingredient_index count*ones(1,num_strln_samples)];
+                usedPoints = [usedPoints quadIngredient{1}.contour.startPoint...
+                                quadIngredient{1}.contour.endPoint];
+                % two points, so store index twice
+                quadIngredientIndices = [quadIngredientIndices quadIndex quadIndex];
+            elseif strcmp(quadIngredient{1}.type,'strLn')
+            % if straight line contour
+                usedPoints = [usedPoints linspace(quadIngredient{1}.a, ...
+                                quadIngredient{1}.b, num_strln_samples)];
+                % store index for each point in the below vector
+                quadIngredientIndices = [quadIngredientIndices ...
+                                        quadIndex*ones(1,num_strln_samples)];
             end
         end
     end
 
-    count = 0;
-    max_val = max(abs(exp(1i*freq*g(used_points))));
-    for n=1:length(quadIngredients)
-        relevant_CPs = used_points(quad_ingredient_index==n);
-        keep_bits = false;
-        % if any bits of the quad ingredient are above the threshold, keep
-        % the entire integreient
-        for SP = relevant_CPs
-            if max_val*thresh <= abs(exp(1i*freq*g(SP)))
-                keep_bits = true;
+    % get estimate for max value of integrand along deformed path
+    maxIntegrand = max(abs(exp(1i*freq*phaseFunction(usedPoints))));
+    % reset index counter, now use for quadrature ingredients which aren't
+    % discarded:
+    quadIndex = 0;
+    for iQuadIngredient = 1:length(QuadIngredientArray)
+        % filter usedPoints which are relevant to this contour/ingredient
+        relevantPoints = usedPoints(quadIngredientIndices == iQuadIngredient);
+        keepIngredient = false;
+
+        % if, for any relevantPoints, the integrand is above the threshold,
+        % keep the entire integrient
+        for iPoints = relevantPoints
+            % check if value at key points is non-negligable compared with
+            % elsewhere, if so, keep ingredient
+            if maxIntegrand*thresh <= abs(exp(1i*freq*phaseFunction(iPoints)))
+                keepIngredient = true;
             end
         end
-        if keep_bits
-            count = count + 1;
-            quadIngredients_out{count} = quadIngredients{n};
+
+        if keepIngredient
+            % add to output
+            quadIndex = quadIndex + 1;
+            QuadIngredientArrayOut{quadIndex} = QuadIngredientArray{iQuadIngredient};
         end
     end
 end
