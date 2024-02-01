20d904ef6516ee0adf40c04823266324e15ef825
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2024-02-01 13:04:00 +0000

    (second) logging fix, addressing Octave warnings

--- src/logging/logQuadratureDetails.m
+++ src/logging/logQuadratureDetails.m
@@ -1,7 +1,7 @@
 function logQuadratureDetails(params, netwonIterationData, quadToc)
     params.log.addToLog(sprintf("Quadrature allocation time:\t%fs",quadToc));
     for i = 1:length(netwonIterationData)
-        newtonSummaryString = [];
+        newtonSummaryString = '';
         for n = (netwonIterationData{i}.')
             newtonSummaryString = ...
                 [newtonSummaryString, ' ', num2str(n)];
