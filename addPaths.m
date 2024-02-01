9ab7018154dbcb22c277c8dd0c8f081960036cd0
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2024-02-01 12:59:12 +0000

    (first) attempt to fix logging test issue

--- addPaths.m
+++ addPaths.m
@@ -2,5 +2,6 @@ function addPaths()
     % add paths required for PathFinder
     pathToRoot = fileparts(which('addPaths.m'));
     pathToSrc = [pathToRoot '/src/'];
+    addpath(pathToRoot);
     addpath(genpath(pathToSrc));
 end
