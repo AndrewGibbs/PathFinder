b5c37f3233f023e26a6e09855863d067c0ca24bf
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2024-02-01 13:09:47 +0000

    (third) logging fix, aiming for more error details

--- test/logTest.m
+++ test/logTest.m
@@ -1,15 +1,15 @@
 function isPass = logTest()
 %Tests that PathFinder can produce a log file without causing an error 
     isPass = true;
-    try
-        % check basic logging
-        PathFinder(-1, 1, @(x) x.^2, [1 -0.5 0.5 0 -1 0], 50, 10, 'log');
+%     try
+    % check basic logging
+    PathFinder(-1, 1, @(x) x.^2, [1 -0.5 0.5 0 -1 0], 50, 10, 'log');
 
-        % check logging with user-specified filename
-        PathFinder(-1, 1, @(x) x.^2, [1 -0.5 0.5 0 -1 0], 50, 10, 'log',...
-            'log name', 'loggletest');
-    catch ERR
-        isPass = false;
-    end
+    % check logging with user-specified filename
+    PathFinder(-1, 1, @(x) x.^2, [1 -0.5 0.5 0 -1 0], 50, 10, 'log',...
+        'log name', 'loggletest');
+%     catch ERR
+%         isPass = false;
+%     end
     close all;
 end
