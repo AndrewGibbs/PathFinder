2e2e63b0c320c6d121d2bb4387dc5b25dd5321b5
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2024-04-18 16:26:08 +0100

    added auto compiler switch option, for CI

--- test/Test_all.m
+++ test/Test_all.m
@@ -2,7 +2,8 @@
 mex -setup C;
 if ispc
   % mingw64XmlPath = fullfile(matlabroot, 'bin', 'win64', 'mexopts', 'mingw64.xml');
-  mex -setup:D:\hostedtoolcache\windows\MATLAB\2024.1.999\x64\bin\win64\mexopts\mingw64.xml C
+%   mex -setup:D:\hostedtoolcache\windows\MATLAB\2024.1.999\x64\bin\win64\mexopts\mingw64.xml C
+    switchWindowsCompiler();
   % mex('-setup', mingw64XmlPath, 'C');
   mex -setup C;
 end
