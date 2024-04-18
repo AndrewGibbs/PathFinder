7e2c9c612de851f9c3916eec760c47dc8a6d2686
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2024-04-18 16:26:08 +0100

    added auto compiler switch option, for CI

---
+++ src/mex_compilation/switchWindowsCompiler.m
@@ -0,0 +1,21 @@
+function switchWindowsCompiler()
+    % Get a list of installed compilers
+    compilers = mex.getCompilerConfigurations('C');
+    
+    % Find the MinGW-w64 compiler
+    mingw_compiler = [];
+    for i = 1:numel(compilers)
+        if contains(compilers(i).Name, 'MinGW-w64')
+            mingw_compiler = compilers(i);
+            break;
+        end
+    end
+    
+    if isempty(mingw_compiler)
+        error('MinGW-w64 compiler not found.');
+    else
+        % Set MinGW-w64 as the active compiler
+        mex('-setup', mingw_compiler.MexOpt);
+        disp('MinGW-w64 compiler set successfully.');
+    end
+end
