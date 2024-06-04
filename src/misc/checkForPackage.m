a5719628eedd36e5c3ee3435c3e48dbde8d0160c
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2024-06-04 09:17:41 +0100

    adjusted check for symbolic math toolbox

---
+++ src/misc/checkForPackage.m
@@ -0,0 +1,12 @@
+function isIncluded = checkForPackage(packageName)
+% checks if a particular package is installed 
+    packages = ver;
+    isIncluded = false;
+    for ip = packages
+        % check if name of element matches desired package
+        if strcmp(packages.Name, packageName)
+            isIncluded = true;
+            break;
+        end
+    end
+end
